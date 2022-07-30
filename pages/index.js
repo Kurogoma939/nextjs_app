6e98df3285f029ff4e3cb664e968173220faef58
Kurogoma939 <kan.sss.939@gmail.com>
2022-07-30 23:42:51 +0900

    importにエイリアス設定

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Header from '../components/header';
-import Hero from '../components/hero';
-import Footer from '../components/footer';
+import Header from '@components/header';
+import Hero from '@components/hero';
+import Footer from '@components/footer';
 
 export default function Home() {
   const subtitle = 'アウトプットするサイト';
