5159dbc5ece02bc351d1cbbe09e00b829c990061
Kurogoma939 <kan.sss.939@gmail.com>
2022-07-31 00:01:20 +0900

    複数ページと画面遷移

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Hero from "@components/hero";
+
+export default function About() {
+    return (
+        <>
+            <a href="/blog"><h1>About</h1></a>
+            <Hero />
+        </>
+
+    );
+}
---
+++ pages/blog/index.js
@@ -0,0 +1,10 @@
+import Hero from "@components/hero";
+
+export default function Blog() {
+    return (
+        <>
+            <a href="/about"><h1>Blog</h1></a>
+            <Hero />
+        </>
+    );
+}
