b74c133f7b42c08105bbf5ed5e2a7d087c7f3bb5
Kurogoma939 <kan.sss.939@gmail.com>
2022-07-31 23:04:05 +0900

    スタイルの設定

--- components/hero.js
+++ components/hero.js
@@ -1,8 +1,10 @@
+import styles from '../styles/hero.module.css'
+
 export default function Hero({ title, subtitle, imageOn = false }) {
     return (
-        <header>
-            <h1>{title}</h1>
-            <p>{subtitle}</p>
+        <header className={styles.text}>
+            <h1 className={styles.title}>{title}</h1>
+            <p className={styles.subtitle}>{subtitle}</p>
             {imageOn && <figure>[画像]</figure>}
         </header>
     )
